1593cefcddffd7a4ba3ef43bdc7040be97940c0b
18782249841 <18782249841@163.com>
2017-10-22 16:45:33 +0800

    it still have mistakes,ai

--- calculator.py
+++ calculator.py
@@ -6,36 +6,39 @@ import csv
 
 class Config(object):
     def __init__(self, configfile):
+        self.configfile = configfile
         self.config = {}
         fd = open(configfile, 'r')
         for line in fd.readlines():
             tmp = line.split('=')
-            self.config[tmp[0].strip()] = float(tmp[1].strip())
+            #self.config[tmp[0].strip()] = float(tmp[1].strip())
+            name = tmp[0].strip()
+            value = tmp[1].strip()
+            self.config[name] = float(value)
 
     def get_config(self, config_key):
         return self.config[config_key]
 
-
 class UserData(object):
-    def __init__(self, userdata):
+    def __init__(self, userdatafile):
         self.userdata = {}
         fd = open(userdata, 'r')
         for line in fd.readlines():
             tmp = line.split(',')
-            self.userdata[tmp[0].strip()] = float(tmp[1].strip())
-
-    def get_config(self, config_key):
-        return self.config[config_key]
-
-    def dumptofile(self, configfile, outfile):
-        with open(outfile, 'a') as file:
-            write = csv.writer(Calculator.output)
-            write.writerows(outfile)
-
+            #self.userdata[tmp[0].strip()] = float(tmp[1].strip())
+            num = tmp[0].strip()
+            salary = tmp[1].strip()
+            self.userdata[num] = salary
 
 class Calculator(object):
-    def taxpayable(self, salary, insurance):
-        taxable_income = salary - float(insurance) - 3500
+    def __init__(self, userdatafile):
+        self.userdata = {}
+
+
+    def taxable_income(self, salary, insurance):
+        return salary - float(insurance) - 3500
+
+    def taxpayable(self, taxable_income):
         tax_payable = 0
         if taxable_income < 0:
             tax_payable = 0
@@ -55,24 +58,26 @@ class Calculator(object):
             tax_payable = (taxable_income * 0.450) - 13505
         return tax_payable
 
-    def min_max(self, num, salary, sum_rate):
-        min_insurance = Config.get_config('JiShuL')
-        max_insurance = Config.get_config('JiShuH')
-        for num, salary in UserData.userdata.items():
-            if salary < min_insurance:
-                insurance = min_insurance * sum_rate()
-            elif salary > max_insurance:
-                insurance = max_insurance * sum_rate
-            else:
-                insurance = salary * sum_rate
-            return insurance
 
+    def min_max(self, num, salary):
+        min_insurance = float(Config.get_config("JiShuL"))
+        max_insurance = float(Config.get_config('JiShuH'))
+        #for num, salary in UserData.userdata.items():
+        if salary < min_insurance:
+            insurance = min_insurance * 0.165
+        elif salary > max_insurance:
+            insurance = max_insurance * 0.165
+        else:
+            insurance = salary * 0.165
+        return insurance
+'''
     def sum_rate(self):
         rate_name = ['YangLao', 'YiLiao', 'ShiYe', 'GongShang', 'ShengYu', 'GonJiJin']
         sum_rate = 0
         for i in rate_name:
             sum_rate = Config.get_config(i) + sum_rate
         return sum_rate
+'''
 
     def final_salary(self, num, salary):
         insurance = self.min_max(salary)
@@ -81,6 +86,17 @@ class Calculator(object):
 
         output = "{},{},{:.2f},{:.2f},{:.2f}".format(num, salary, insurance, tax_payable, final_salary)
         # print(output)
+        self.output += output
+
+    output = ''
+    userdata = {}
+    for i in userdata:
+        final_salary(i,int(userdata[i]))
+
+    def dumptofile(self, outfile):
+        with open(outfile, 'a') as file:
+            file.write(self.output)
+
 
 
 if __name__ == '__main__':
@@ -93,9 +109,10 @@ if __name__ == '__main__':
     outfile = args[index_o + 1]
 
     config = Config(configfile)
+    user = UserData(userdata)
+    user = Calculator(userdata)
+    user = UserData(outfile)
 
-    userdata = UserData(user)
-    userdata.dumptofile(confile, output)
 
 
 
