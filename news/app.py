064a0eef57d929b5e0cd0a95157760d3ff4dcefc
Sophie521 <18782249841@163.com>
2017-10-29 22:16:45 +0800

    challage006

--- news/app.py
+++ news/app.py
@@ -3,20 +3,21 @@ import os
 import json
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
     with open('/home/shiyanlou/news/files/helloshiyanlou.json') as file:
-        text1 = json.loads(f.read())
+        text1 = json.loads(file.read())
     with open('/home/shiyanlou/news/files/helloworld.json') as file:
-        text2 = json.loads(f.read())   
+        text2 = json.loads(file.read())   
     return render_template('index.html',text1 = text1,text2 = text2)
 
 @app.route('/files/<filename>')
 def file(filename):
     with open('/home/shiyanlou/news/files/{}.json'.format(filename)) as file:
-        file_item = json.loads(f.read())
-    return render_templates('file.html',file_item = file_item)
+        text = json.loads(file.read())
+    return render_templates('file.html',text = text)
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
